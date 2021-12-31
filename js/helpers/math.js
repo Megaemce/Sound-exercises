617414cd0fe4ff30cf3d8c1e17ec645b1194996f
Megaemce <1651451+Megaemce@users.noreply.github.com>
2021-12-31 14:38:59 +0100

    Readd the function from math.js

--- js/helpers/math.js
+++ js/helpers/math.js
@@ -1,3 +1,32 @@
+/* from log slider position to real value */
+export function logPositionToValue(position, valueMin, valueMax) {
+    const min = Math.abs(parseFloat(valueMin)) || 1;
+    const max = Math.abs(parseFloat(valueMax)) || 1;
+    const minSign = valueMin < 0 ? -1 : 1;
+    const maxSign = valueMax < 0 ? -1 : 1;
+    if (maxSign * max - minSign * min === 0) return 0;
+    // calculate adjustment factor
+    const scale = (maxSign * Math.log(max) - minSign * Math.log(min)) / (maxSign * max - minSign * min);
+    const calculation = Math.exp((position - minSign * min) * scale + minSign * Math.log(min));
+
+    return calculation;
+}
+
+/* from real value to log slider position */
+export function valueToLogPosition(sliderValue, sliderMin, sliderMax) {
+    // calculate adjustment factor
+    const max = parseFloat(sliderMax);
+    const min = Math.abs(parseFloat(sliderMin)) || 1;
+    const value = Math.abs(parseFloat(sliderValue)) || 1;
+    const maxSign = sliderMax < 0 ? -1 : 1;
+    const minSign = sliderMin < 0 ? -1 : 1;
+    const valueSign = sliderValue < 0 ? -1 : 1;
+
+    const scale = (maxSign * Math.log(max) - minSign * Math.log(min)) / (maxSign * max - minSign * min);
+    const calculation = minSign * min + (valueSign * Math.log(value) - minSign * Math.log(min)) / scale;
+
+    return calculation;
+}
 /* returns direction as a string based on line between point A and B */
 export function directionString(pointA, pointB) {
     let angle = (Math.atan2(pointB.y - pointA.y, pointB.x - pointA.x) * 180) / Math.PI;
@@ -14,4 +43,14 @@ export function directionString(pointA, pointB) {
     }
 
     return false;
+}
+/* scale value between [from_min,from_max] to [to_min,to_max] range */
+export function scaleBetween(value, from_min, from_max, to_min, to_max) {
+    const calculation = ((value - from_min) * (to_max - to_min)) / (from_max - from_min) + to_min;
+
+    return calculation;
+}
+/* scale dB to linear values */
+export function dBToLinear(db) {
+    return Math.pow(10.0, 0.05 * db);
 }
