9f4005678af5fb4adb618ad1d3fc70c1c87509e8
Magdalena Paluch <magdalena.paluch@chop-chop.org>
2022-01-08 17:40:28 +0100

    change styles

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ import output from "./effects/output.js";
 export let audioContext;
 
 export function setPlayingVariable(state) {
-    playing = state;
+	playing = state;
 }
 
 export let playing; // general bool to know if something is playing
@@ -24,99 +24,114 @@ const startButton = document.getElementById("start_button");
 const startScreen = document.getElementById("start_screen");
 
 function buildConnection() {
-    outputNode = output();
-    equalizerANode = equalizer("equalizerA");
-    equalizerBNode = equalizer("equalizerB");
+	outputNode = output();
+	equalizerANode = equalizer("equalizerA");
+	equalizerBNode = equalizer("equalizerB");
 
-    const outputLeft = outputNode.audioNode.leftNode;
-    const outputRight = outputNode.audioNode.rightNode;
-    const outputOutput = outputNode.audioNode.outputNode;
-    const equalizerAInput = equalizerANode.audioNode.inputNode;
-    const equalizerAOutput = equalizerANode.audioNode.outputNode;
-    const equalizerBInput = equalizerBNode.audioNode.inputNode;
-    const equalizerBOutput = equalizerBNode.audioNode.outputNode;
+	const outputLeft = outputNode.audioNode.leftNode;
+	const outputRight = outputNode.audioNode.rightNode;
+	const outputOutput = outputNode.audioNode.outputNode;
+	const equalizerAInput = equalizerANode.audioNode.inputNode;
+	const equalizerAOutput = equalizerANode.audioNode.outputNode;
+	const equalizerBInput = equalizerBNode.audioNode.inputNode;
+	const equalizerBOutput = equalizerBNode.audioNode.outputNode;
 
-    inputNode = input(equalizerAInput, equalizerBInput);
+	inputNode = input(equalizerAInput, equalizerBInput);
 
-    outputOutput.connect(audioContext.destination);
-    equalizerAOutput.connect(outputLeft);
-    equalizerBOutput.connect(outputRight);
+	outputOutput.connect(audioContext.destination);
+	equalizerAOutput.connect(outputLeft);
+	equalizerBOutput.connect(outputRight);
 
-    outputNode.audioNode.switchTo("left");
+	outputNode.audioNode.switchTo("left");
 
-    // if something was playing before don't stop it
-    if (inputPrevState) {
-        inputNode.playSound();
-    }
+	// if something was playing before don't stop it
+	if (inputPrevState) {
+		inputNode.playSound();
+	}
 
-    equalizerADiv.onclick = () => {
-        equalizerADiv.classList.remove("question");
-    };
+	equalizerADiv.querySelector(".content").onclick = () => {
+		equalizerADiv.classList.remove("question");
+	};
+	equalizerADiv.querySelector(".head .reset").onclick = () => {
+		equalizerADiv.classList.add("question-next");
+		setTimeout(() => {
+			equalizerADiv.classList.remove("question-next");
+		}, 2000);
+	};
+	equalizerADiv.querySelector(".head .config").onclick = () => {
+		equalizerADiv.classList.contains("show-config")
+			? equalizerADiv.classList.remove("show-config")
+			: equalizerADiv.classList.add("show-config");
+	};
 }
 
 function destroyConnection() {
-    inputPrevState = inputNode.isTransmitting;
+	inputPrevState = inputNode.isTransmitting;
 
-    inputNode.stopSound();
-    outputNode.audioNode.outputNode.disconnect();
-    equalizerANode.audioNode.outputNode.disconnect();
-    equalizerBNode.audioNode.outputNode.disconnect();
+	inputNode.stopSound();
+	outputNode.audioNode.outputNode.disconnect();
+	equalizerANode.audioNode.outputNode.disconnect();
+	equalizerBNode.audioNode.outputNode.disconnect();
 
-    equalizerANode.resetEqualizer();
-    equalizerBNode.resetEqualizer();
+	equalizerANode.resetEqualizer();
+	equalizerBNode.resetEqualizer();
 
-    equalizerADiv.classList.add("question");
+	equalizerADiv.classList.add("question");
 }
 
 export function deactiveAllCables() {
-    cableAOuput.setAttribute("stroke", "black");
-    cableBOutput.setAttribute("stroke", "black");
-    cableInputA.setAttribute("stroke", "black");
-    cableInputB.setAttribute("stroke", "black");
+	cableAOuput.setAttribute("stroke", "black");
+	cableBOutput.setAttribute("stroke", "black");
+	cableInputA.setAttribute("stroke", "black");
+	cableInputB.setAttribute("stroke", "black");
 }
 
 export function activeAllCables() {
-    if (inputNode.isTransmitting) {
-        if (outputNode.track === "B") {
-            cableInputB.setAttribute("stroke", "url(#grad-right-to-left)");
-            cableBOutput.setAttribute("stroke", "url(#grad-right-to-left)");
-            cableAOuput.setAttribute("stroke", "black");
-            cableInputA.setAttribute("stroke", "black");
-        }
-        if (outputNode.track === "A") {
-            cableInputA.setAttribute("stroke", "url(#grad-top-to-bottom)");
-            cableAOuput.setAttribute("stroke", "url(#grad-left-to-right)");
-            cableBOutput.setAttribute("stroke", "black");
-            cableInputB.setAttribute("stroke", "black");
-        }
-    }
+	if (inputNode.isTransmitting) {
+		if (outputNode.track === "B") {
+			cableInputB.setAttribute("stroke", "url(#grad-right-to-left)");
+			cableBOutput.setAttribute("stroke", "url(#grad-right-to-left)");
+			cableAOuput.setAttribute("stroke", "black");
+			cableInputA.setAttribute("stroke", "black");
+		}
+		if (outputNode.track === "A") {
+			cableInputA.setAttribute("stroke", "url(#grad-top-to-bottom)");
+			cableAOuput.setAttribute("stroke", "url(#grad-left-to-right)");
+			cableBOutput.setAttribute("stroke", "black");
+			cableInputB.setAttribute("stroke", "black");
+		}
+	}
 }
 
 // start audio with user interaction (chrome policy)
 startButton.onclick = () => {
-    try {
-        audioContext = new AudioContext();
-    } catch (e) {
-        alert("The Web Audio API is not supported in this browser.");
-    }
+	try {
+		audioContext = new AudioContext();
+	} catch (e) {
+		alert("The Web Audio API is not supported in this browser.");
+	}
 
-    startScreen.classList.add("hidden");
+	startScreen.classList.add("hidden");
 
-    audioContext.nameSoundBuffer = new Object();
+	audioContext.nameSoundBuffer = new Object();
 
-    // build everything when all the noise are loaded
-    audioContext.audioWorklet.addModule("js/effects/whiteNoise.js").then(() => {
-        audioContext.audioWorklet.addModule("js/effects/pinkNoise.js").then(() => {
-            audioContext.audioWorklet.addModule("js/effects/brownNoise.js").then(() => {
-                buildConnection();
-            });
-        });
-    });
+	// build everything when all the noise are loaded
+	audioContext.audioWorklet.addModule("js/effects/whiteNoise.js").then(() => {
+		audioContext.audioWorklet
+			.addModule("js/effects/pinkNoise.js")
+			.then(() => {
+				audioContext.audioWorklet
+					.addModule("js/effects/brownNoise.js")
+					.then(() => {
+						buildConnection();
+					});
+			});
+	});
 
-    document.getElementById("apply-button").onclick = () => {
-        destroyConnection();
-        buildConnection();
-    };
+	document.getElementById("apply-button").onclick = () => {
+		destroyConnection();
+		buildConnection();
+	};
 
-    document.onmousemove = undefined;
+	document.onmousemove = undefined;
 };
